586e3f03fd15cf6df7539e7415ae42b2a4175f25
Yeheyis Getachew <getgetay@gmail.com>
2021-07-16 11:07:49 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase'
+var firebaseConfig = {
+  apiKey: "AIzaSyCgRfqixfiH-Z4QgWqcFixZvMEs9CfF6F4",
+  authDomain: "shopei-2e181.firebaseapp.com",
+  projectId: "shopei-2e181",
+  storageBucket: "shopei-2e181.appspot.com",
+  messagingSenderId: "86529059772",
+  appId: "1:86529059772:web:9b3b6bce6aa582772519b9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
